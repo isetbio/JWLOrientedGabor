ad03cdee5bbb4ad1f5008f44df62380b742aece7
elinekupers <elinekupers@gmail.com>
2018-03-12 18:49:10 -0400

    update hpc bash script

--- HPC/runme_hpc_computeabsorptions_hex.sh
+++ HPC/runme_hpc_computeabsorptions_hex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 ##SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --time 10:00:00 # time (D-HH:MM)
 #SBATCH --mem=125GB # memory pool for all cores
 #SBATCH --job-name=s_ogRGC_hex
