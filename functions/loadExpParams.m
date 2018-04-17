281b12e2266c5cf44d05fe9cc0ca8454f50058e8
elinekupers <elinekupers@gmail.com>
2018-04-17 13:44:57 -0400

    load eccentricity based cone spacing from banks et al 1991 to use in absorption computation

--- functions/loadExpParams.m
+++ functions/loadExpParams.m
@@ -101,7 +101,7 @@ switch expName
     case 'eccBasedConeSpacing'
         expParams.contrastLevels  = [0:0.005:0.04, 0.05:0.01:.1]; % Stimulus contrast levels (Michelson)
         expParams.eyemovement     = [1 1 0]';         % Which type of eye movments, emFlag will be turned into doubling amplitude or speed
-        expParams.eccentricities  = [0, 2.5, 5, 10, 20, 40];              % Eccentricity (deg);
+        expParams.eccentricities  = [2.5, 10, 40];%[0, 2.5, 5, 10, 20, 40];              % Eccentricity (deg);
         expParams.spatFreq        = 4;                % Spatial frequency (cycles/deg);
         expParams.polarAngle      = 0;                % Polar angle (radians): 0 is right, pi/2 is superior, pi is left, 3*pi/2 inferior
         expParams.defocusLevels   = 0;                % Value of first Zernike coeff (= defocus in units of ??)         
