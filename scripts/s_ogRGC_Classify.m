960b1ac6a840099b14d223c857d01fef1da6bd8c
elinekupers <elinekupers@gmail.com>
2018-04-18 19:28:04 -0400

    debug parfor loop in classify script

--- scripts/s_ogRGC_Classify.m
+++ scripts/s_ogRGC_Classify.m
@@ -33,7 +33,7 @@ set(gca, 'XScale','log', 'XLim', [.005 max(expParams.contrastLevels)], 'XTick', 
 ylabel('Classifier Accuracy')
 xlabel('Contrast level (Michelson)')
 
-parfor eccen = expParams.eccentricities 
+parfor eccen = 1:nrEccen
     for df = 1:nrDefocusLevels
         for em = 1:max(nrEyemovTypes)
             for sf = expParams.spatFreq
@@ -45,7 +45,7 @@ parfor eccen = expParams.eccentricities
                     % Load dataset
                     fname = sprintf(...
                         'OGconeOutputs_contrast%1.3f_pa%d_eye%s_eccen%1.2f_defocus%1.2f_noise-random_sf%1.2f.mat',...
-                            c,expParams.polarAngle,sprintf('%i',expParams.eyemovement(:,em)), eccen, expParams.defocusLevels(df), sf);
+                            c,expParams.polarAngle,sprintf('%i',expParams.eyemovement(:,em)), expParams.eccentricities(eccen), expParams.defocusLevels(df), sf);
                     
                     if currentFlag
                         fname = ['current_' fname];
@@ -126,7 +126,7 @@ parfor eccen = expParams.eccentricities
                 % Save classifier accuracy
                 fname = sprintf(...
                     'Classify_coneOutputs_contrast%1.3f_pa%d_eye%s_eccen%1.2f_defocus%1.2f_noise-random_sf%1.2f',...
-                        c, expParams.polarAngle,sprintf('%i',expParams.eyemovement(:,em)), eccen, expParams.defocusLevels(df), sf);
+                        c, expParams.polarAngle,sprintf('%i',expParams.eyemovement(:,em)), expParams.eccentricities(eccen), expParams.defocusLevels(df), sf);
                 if currentFlag; fname = ['current_' fname]; end               
                 parsave(fullfile(savePth, sprintf('%s.mat', fname)),'P',P)
                 
