2c7bebdfe06054e1870e133afcde5f8bbd5c28c7
elinekupers <elinekupers@gmail.com>
2018-05-15 12:22:23 -0400

    update comments in absorption video script

--- scripts/s_createAbsorptionVideo.m
+++ scripts/s_createAbsorptionVideo.m
@@ -1,21 +1,27 @@
-vname = '~/Desktop/absorptions220';
+% s_createAbsorptionVideo.m
+
+% Get video name and create a video object
+vname = '~/Desktop/absorptions110_C0.10';
 vObj = VideoWriter(vname);
+
+% Define frame rate
 vObj.FrameRate = 30;
+
+% Open video object
 open(vObj);
 
-
+% Plot absorptions (one frame = one time step) and write into video object
 hFig = figure;
-colormap(gray(max(absorptions(:))));
+maxRate = max(absorptions(:));
+colormap(gray(maxRate));
 for ii=1:size(absorptions,4)
     
-    imagesc(squeeze(absorptions(1,:,:,ii,1))); axis image; axis off; box off; drawnow;
-    
-    
-    
+    imagesc(squeeze(absorptions(1,:,:,ii,1))); set(gca,'CLim',[0 maxRate]);
+    axis image; axis off; box off; drawnow;
+
     F = getframe; writeVideo(vObj,F);
 end
 
-% Write the video object if save is true
-
+% Save and close the video file
 writeVideo(vObj,F);
 close(vObj);
