e223019c4d03c37346881b60f253490c114115b2
elinekupers <elinekupers@gmail.com>
2018-06-06 13:49:55 -0400

    add hpc bash script to run defocus classification with training on high contrast only

---
+++ HPC/runme_hpc_classify_parfor_blur_trainHighContrast.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time 20:00:00 # time (D-HH:MM)
+#SBATCH --mem=125GB # memory pool for all cores
+#SBATCH --job-name=s_ogRGC_Classify_parfor
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ek99@nyu.edu
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+
+module load matlab/2016b
+
+cd /scratch/ek99/JWLOrientedGabor/
+
+# If the files you are running are not in the same folder as this script,
+# you can insert "addpath(genpath('/PATH/TO/FILES/'));" before the command
+# you want to run.
+matlab -nodisplay -r "addpath(genpath('/scratch/ek99/JWLOrientedGabor')); addpath(genpath('/scratch/ek99/isetbio')); s_ogRGC_Classify_HPC_blur_trainHighContrast; exit()"
+
+exit
+
