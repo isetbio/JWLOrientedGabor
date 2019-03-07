339682d572680ac4adeb80a0e0f85d2d75245fcf
elinekupers <elinekupers@gmail.com>
2019-03-07 11:04:21 -0500

    fix bug in folder name

--- HPC/prepHPC.m
+++ HPC/prepHPC.m
@@ -1,4 +1,4 @@
-function [expName, subFolder, seed] = prepHPC(taskID)
+function [expName, saveFolder, seed] = prepHPC(taskID)
 
 % function to run different experiments with different seeds on NYU's HPC
 
@@ -8,27 +8,27 @@ switch taskID
     
     case {1, 2, 3, 4, 5}
         expName   = 'defocus';
-        subFolder = sprintf('run%d', taskID);
+        saveFolder = sprintf('run%d', taskID);
         seed      = taskID;
         
     case {6, 7, 8, 9, 10}
         expName   = 'conedensity';
-        subFolder = sprintf('run%d', taskID-5);
+        saveFolder = sprintf('run%d', taskID-5);
         seed      = taskID-5;
         
     case {11, 12, 13, 14, 15}
         expName   = 'eyemov';
-        subFolder = sprintf('run%d', taskID-10);
+        saveFolder = sprintf('run%d', taskID-10);
         seed      = taskID-10;
         
     case {16, 17, 18, 19, 20}
         expName   = 'conetypes';
-        subFolder = sprintf('run%d', taskID-15);
+        saveFolder = sprintf('run%d', taskID-15);
         seed      = taskID-15;
         
     case {21, 22, 23, 24, 25}
         expName   = 'conetypesmixed';
-        subFolder = sprintf('run%d', taskID-20);
+        saveFolder = sprintf('run%d', taskID-20);
         seed      = taskID-20;
 end
 
