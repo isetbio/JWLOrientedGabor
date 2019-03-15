192c5fb98872c092ea1ffbdcc0e0769eeb74d20e
elinekupers <elinekupers@gmail.com>
2019-03-15 18:15:25 -0400

    add ideal observer functions

---
+++ functions/plotPsychometricFunctionsIdeal.m
@@ -0,0 +1,204 @@
+function [] = plotPsychometricFunctionsIdeal(expName, varargin)
+
+
+% Function to compute psychometric functions based on the computational
+% observer model.
+
+% INPUTS:
+% expName         : string defining the condition you want to plot.
+%                   (See load expParams for possible conditions)
+% [subFolderName] : string defining the sub folder you want to plot from.
+
+% [saveFig]       : boolean defining to save figures or not
+% [plotAvg]      : boolean defining to plot average across experiments runs or not
+
+%% 0. Set general experiment parameters
+p = inputParser;
+p.KeepUnmatched = true;
+p.addRequired('expName', @ischar);
+p.addParameter('subFolderName1', 'idealtemplate', @ischar);
+p.addParameter('subFolderName2', 'idealsimulation', @ischar);
+p.addParameter('subFolderName3', 'idealsvm', @ischar);
+
+p.addParameter('saveFig', false, @islogical);
+p.parse(expName, varargin{:});
+
+% Rename variables
+expName       = p.Results.expName;
+subFolderName{1} = p.Results.subFolderName1;
+subFolderName{2} = p.Results.subFolderName2;
+subFolderName{3} = p.Results.subFolderName3;
+saveFig       = p.Results.saveFig;
+
+% Load specific experiment parameters
+expParams                   = loadExpParams(expName, false);
+[xUnits, colors, labels, M, lineStyles] = loadWeibullPlottingParams(expName);
+
+% Define figurePth
+figurePth   = fullfile(ogRootPath,'figs', expName, subFolderName{1});
+
+% Number of total trials in computational observer model (50 clockwise, 50 counterclockwise)
+nTotal      = expParams.nTrials;
+
+%% 1. Set Weibull fitting parameters
+
+% Prepare fit variables
+fit = [];
+
+% Predefine cell arrays
+fit.ctrpred = cell(size(colors,1),1);
+fit.ctrvar  = cell(size(colors,1),1);
+fit.ctrr2   = cell(size(colors,1),1);
+fit.data    = cell(size(colors,1),1);
+
+% Set inital slope, threshold for first stage fitting
+fit.thresh = 0.75;
+
+%% 2. Load classification accuracy
+
+% Load first filename
+fName{1}   = sprintf('ideal_Classify_coneOutputs_contrast%1.3f_pa0_eye%s_eccen%1.2f_defocus%1.2f_noise-none_sf%1.2f_lms-%1.1f%1.1f%1.1f.mat', ...
+    max(expParams.contrastLevels), ...
+    sprintf('%i',expParams.eyemovement'), ...
+    expParams.eccentricities, ...
+    expParams.defocusLevels, ...
+    expParams.spatFreq, ...
+    expParams.cparams.spatialDensity(1,2), ...
+    expParams.cparams.spatialDensity(1,3), ...
+    expParams.cparams.spatialDensity(1,4));
+
+% Load first filename
+fName{2}   = sprintf('ideal_Classify_coneOutputs_contrast%1.3f_pa0_eye%s_eccen%1.2f_defocus%1.2f_noise-random_sf%1.2f_lms-%1.1f%1.1f%1.1f.mat', ...
+    max(expParams.contrastLevels), ...
+    sprintf('%i',expParams.eyemovement'), ...
+    expParams.eccentricities, ...
+    expParams.defocusLevels, ...
+    expParams.spatFreq, ...
+    expParams.cparams.spatialDensity(1,2), ...
+    expParams.cparams.spatialDensity(1,3), ...
+    expParams.cparams.spatialDensity(1,4));
+
+% Load first filename
+fName{3}   = sprintf('Classify_coneOutputs_contrast%1.3f_pa0_eye%s_eccen%1.2f_defocus%1.2f_noise-random_sf%1.2f_lms-%1.1f%1.1f%1.1f_AVERAGE.mat', ...
+    max(expParams.contrastLevels), ...
+    sprintf('%i',expParams.eyemovement'), ...
+    expParams.eccentricities, ...
+    expParams.defocusLevels, ...
+    expParams.spatFreq, ...
+    expParams.cparams.spatialDensity(1,2), ...
+    expParams.cparams.spatialDensity(1,3), ...
+    expParams.cparams.spatialDensity(1,4));
+
+fNameSE   = sprintf('Classify_coneOutputs_contrast%1.3f_pa0_eye%s_eccen%1.2f_defocus%1.2f_noise-random_sf%1.2f_lms-%1.1f%1.1f%1.1f_SE.mat', ...
+    max(expParams.contrastLevels), ...
+    sprintf('%i',expParams.eyemovement'), ...
+    expParams.eccentricities, ...
+    expParams.defocusLevels, ...
+    expParams.spatFreq, ...
+    expParams.cparams.spatialDensity(1,2), ...
+    expParams.cparams.spatialDensity(1,3), ...
+    expParams.cparams.spatialDensity(1,4));
+
+dataPth     = fullfile(ogRootPath,'data','PF_data_alias','classification',expName);
+SE{3} = load(fullfile(dataPth, 'svm', fNameSE));
+
+for ii = 1:length(fName)
+    
+    dataPth     = fullfile(ogRootPath,'data','PF_data_alias','classification',expName, subFolderName{ii});
+    if ii == 1
+        fit.init   = [3, 0.0006]; % slope, threshold at ~80%
+    elseif ii == 2
+        fit.init   = [2, 0.005]; % slope, threshold at ~80%
+    elseif ii == 3
+        fit.init   = [2, 0.005]; % slope, threshold at ~80%
+        expParams.contrastLevels =[0:0.001:0.01, 0.015:0.005:0.04, 0.05:0.01:0.1];
+        xUnits =  linspace(min(expParams.contrastLevels),max(expParams.contrastLevels),200);
+    end
+    
+    % load model performance
+    accuracy = load(fullfile(dataPth, fName{ii}));
+    fn = fieldnames(accuracy);
+    accuracy.P = squeeze(accuracy.(fn{1}));
+    
+    % Transpose matrix if necessary
+    if size(accuracy.P,1)<size(accuracy.P,2)
+        accuracy.P = accuracy.P';
+    end
+    
+    %% 3. Fit Weibull
+    % Make a Weibull function first with contrast levels and then search for
+    % the best fit with the classifier data
+    fit.ctrvar{ii} = fminsearch(@(x) ogFitWeibull(x, expParams.contrastLevels, accuracy.P, nTotal), fit.init);
+    
+    % Then fit a Weibull function again, but now with the best fit parameters
+    % from the previous step.
+    fit.ctrpred{ii} = ogWeibull(fit.ctrvar{ii}, xUnits);
+    
+    %% 4. Find contrast threshold
+    %             diff   = abs(fit.ctrpred{count} - fit.thresh);
+    %             minval = find(diff == min(diff));
+    fit.ctrthresh{ii} = fit.ctrvar{ii}(2);
+    fit.data{ii} = accuracy.P;
+    
+end
+
+
+%% 5. Visualize psychometric curves
+
+figure(3); clf; set(gcf,'Color','w', 'Position',  [1000, 850, 986, 488], 'NumberTitle', 'off', 'Name', sprintf('Psychometric function condition: %s', expName)); hold all;
+
+% Remove empty cells
+idx = ~cellfun(@isempty, fit.ctrpred);
+fit.ctrpred = fit.ctrpred(idx);
+
+% Define a zero point (just a very small number), to plot the 0 contrast,
+% since a log-linear plot does not define 0.
+logzero = 1e-5;
+markerColors = colors;
+markerColors(2,:) = [1 1 1];
+
+% Loop over all functions to plot
+for ii = 1:length(fName)
+    
+    % What to plot?
+    dataToPlot = fit.data{ii};
+    fitToPlot  = fit.ctrpred{ii}*100;
+    
+    % Reset contrasts and logzero point for one special case
+    if ii==3
+        expParams.contrastLevels = [0:0.001:0.01, 0.015:0.005:0.04, 0.05:0.01:0.1];
+        xUnits =  linspace(min(expParams.contrastLevels),max(expParams.contrastLevels),200);
+    else
+        expParams.contrastLevels = [0:0.0001:0.001, 0.0015, 0.002, 0.003, 0.004];
+        xUnits =  linspace(min(expParams.contrastLevels),max(expParams.contrastLevels),200);
+    end
+    
+    %
+    plot(xUnits(2:end), fitToPlot(2:end), 'Color', colors(ii,:), 'LineWidth',2, 'LineStyle', lineStyles{ii});
+    scatter(expParams.contrastLevels(2:end), dataToPlot(2:end), 80, colors(ii,:),'MarkerEdgeColor',colors(ii,:), 'MarkerFaceColor', markerColors(ii,:));
+    
+    plot(logzero,dataToPlot(1),'o','Color',colors(ii,:), 'MarkerSize', 8, 'MarkerEdgeColor',colors(ii,:), 'MarkerFaceColor', markerColors(ii,:))
+    
+    if ii ==3
+        errorbar([logzero, expParams.contrastLevels(2:end)], dataToPlot, SE{ii}.P_SE,'Color', colors(ii,:), 'LineStyle','none');
+    end
+end
+
+expParams.contrastLevels = [0:0.0001:0.001, 0.002:0.001:0.01, 0.02:0.01:0.1];
+xmax = 0.04;
+
+set(gca, 'XScale','log','XLim',[logzero, xmax],'YLim', [40 100], 'TickDir','out','TickLength',[.015 .015],'FontSize',17, 'LineWidth',2);
+set(gca, 'XTick', [logzero, expParams.contrastLevels(2), 0.001, 0.01, 0.04], 'XTickLabel',sprintfc('%1.2f',[0 expParams.contrastLevels(2), 0.001, 0.01, 0.04]*100))
+
+ylabel('Classifier Accuracy (% Correct)', 'FontSize',17)
+xlabel('Stimulus Contrast (%)', 'FontSize',17);
+
+h = findobj(gca,'Type','line');
+legend([h(end:-2:2)],labels, 'Location','bestoutside'); legend boxoff
+
+if saveFig
+    if ~exist(figurePth,'dir'); mkdir(figurePth); end
+    savefig(fullfile(figurePth,sprintf('WeibullFit_contrastVSperformance_%s_idealvsSVM',expName)))
+    hgexport(gcf,fullfile(figurePth,sprintf('WeibullFit_contrastVSperformance_%s_idealvsSVM.eps',expName)))
+end
+
---
+++ scripts/s_idealGeislerDPrime.m
@@ -0,0 +1,193 @@
+% s_idealGeislerDPrime
+
+% Load parameters
+expName = 'idealobserver';
+expParams = loadExpParams(expName, false);
+
+% save figures?
+saveFigs = true;
+figure(1); clf; hold all;
+
+for c = expParams.contrastLevels
+    % Get file name 100% contrast (i.e. ideal template)
+    fnameTemplate = sprintf('OGconeOutputs_contrast%1.3f_pa0_eye00_eccen4.50_defocus0.00_noise-none_sf4.00_lms-0.60.30.1.mat',c);
+    
+    % Load data
+    template = load(fullfile(ogRootPath, 'data', expName, 'idealtemplate', fnameTemplate));
+    template = template.absorptions;
+    
+    % Get the trials and samples (should be the data for all data sets though
+    nStimuli = size(template,5);
+    nTrials  = size(template,1) * nStimuli/2;
+    tSamples = size(template,4);
+    nrows    = size(template,2);
+    ncols    = size(template,3);
+    
+    %   permute to trials x stimuli x rows x cols x time points
+    template = permute(template, [1 5 2:4]);
+    
+    %   reshape to (trials x stimuli) x rows x cols x time points
+    template = reshape(template, [], nrows, ncols, tSamples);
+    
+    % Label clockwise and counterclockwise trials
+    label = [ones(nTrials, 1); -ones(nTrials, 1)]; % First set is CW, second set is CCW
+    
+    %% 3. Analytical solution
+    
+    % Get all trials with the same label and only take one trial and one time point
+    % (since data are all the same across time, without any photon noise or eyemovement or
+    % phase shifts)
+    alphaMean = template(label==1,:,:,:);
+    templateCW = alphaMean(1,:,:,1);
+    templateCW = templateCW(:);
+    betaMean = template(label==-1,:,:,:);
+    templateCCW = betaMean(1,:,:,1);
+    templateCCW = templateCCW(:);
+    
+    if templateCW==templateCCW
+        dprimeA(c==expParams.contrastLevels) = 0;
+        percentCorrect(c==expParams.contrastLevels) = 0.5;
+        
+        fprintf('Contrast %1.3f \t d-prime: %2.3f,  percent correct: %2.3f\n', c, dprimeA(c==expParams.contrastLevels),percentCorrect(c==expParams.contrastLevels))
+        plot([1 0.5 0],[1 0.5 0],'LineWidth',4); drawnow;
+    else
+        numerator = sum( (templateCCW-templateCW).*log(templateCCW./templateCW) );
+        denominator = sqrt( 0.5* sum( (templateCW+templateCCW) .* (log(templateCCW./templateCW)).^2 ));
+    
+        dprime = numerator/denominator;
+        
+        criteria = linspace(min([templateCW; templateCCW]), max([templateCW; templateCCW]), 1000);
+        
+        pCW = 1-normcdf(0, dprime, 1);
+        pCCW = 1-pCW;
+
+%         pCW = 1-normcdf(criteria, sum( templateCW.*log(templateCCW./templateCW) ), sqrt(sum( templateCW .* (log(templateCCW./templateCW).^2) )));
+%         pCCW = 1-normcdf(criteria, sum( templateCCW.*log(templateCCW./templateCW) ), sqrt(sum( templateCCW.* (log(templateCCW./templateCW).^2) )));
+        dprimeA(c==expParams.contrastLevels) = dprime;
+        percentCorrect(c==expParams.contrastLevels) = -trapz([1 pCCW 0],[1 pCW 0]);
+%         plot([1 pCCW 0],[1 pCW 0],'LineWidth',4); drawnow;
+        
+        fprintf('Contrast %1.3f \t d-prime: %2.3f,  percent correct: %2.3f\n', c, dprimeA(c==expParams.contrastLevels),percentCorrect(c==expParams.contrastLevels))
+
+    end
+end
+
+saveFolderClassification = fullfile(ogRootPath, 'data', 'classification', expName, 'idealtemplate');
+fnameClassify = sprintf('ideal_Classify_coneOutputs_contrast%1.3f_pa0_eye00_eccen4.50_defocus0.00_noise-none_sf4.00_lms-0.60.30.1', max(expParams.contrastLevels));
+accuracy = percentCorrect.*100;
+parsave(fullfile(saveFolderClassification, sprintf('%s.mat', fnameClassify)),'accuracy',accuracy);
+
+logzero = 4e-4;
+figure; plot(expParams.contrastLevels(2:end), percentCorrect(2:end), 'o-'); hold on;
+plot(logzero, percentCorrect(1), 'o')
+set(gca, 'XScale', 'log', 'YLim', [.4, 1])
+
+%% Simulation
+
+% If absorption data are the same, it doesn't matter..
+if all(templateCW == templateCCW)
+    percentCorrect = 0.5;
+    dPrime = 0;
+    
+    
+else %% classify the template data
+    index = find(templateCW ~= templateCCW);
+    
+    expNameData = 'defaultnophaseshift';
+    expParams = loadExpParams(expNameData, false);
+    subFolderName = 'run1';
+    
+    if saveFigs
+        figurePth = fullfile(ogRootPath, 'figs', expNameData, subFolderName);
+        if ~exist('figurePth','dir'); mkdir(figurePth); end;
+    end
+    
+    for c = expParams.contrastLevels
+        
+        fnameData = sprintf('OGconeOutputs_contrast%1.4f_pa0_eye00_eccen4.50_defocus0.00_noise-random_sf4.00_lms-0.60.30.1.mat', c);
+        data = load(fullfile(ogRootPath, 'data', expNameData, subFolderName, fnameData));
+        data = data.absorptions;
+        
+        %   permute to trials x stimuli x rows x cols x time points
+        data = permute(data, [1 5 2:4]);
+        
+        %   reshape to (trials x stimuli) x rows x cols x time points
+        data = reshape(data, [], nrows, ncols, tSamples);
+        
+        % permute to rows x cols x (trials x stimuli) x time points
+        data  = permute(data, [2 3 1 4]);
+        
+        % reshape to all trials x [rows x colums x time] for classification
+        data = permute(data, [3 1 2 4]);
+        
+        CWData = data(label==1,:,:,1);
+        CWData = CWData(1:200,:);
+        CCWData = data(label==-1,:,:,1);
+        CCWData = CCWData(1:200,:);
+        
+        % Compute loglikelihood per trial
+        for ii = 1:nTrials
+            ZGivenCW(ii)= sum(CWData(ii,:)' .* log(templateCCW./templateCW));
+            ZGivenCCW(ii)= sum(CCWData(ii,:)' .* log(templateCCW./templateCW));
+        end
+        
+        % Compute mean and varianec across trials
+        meanZGivenCCW = mean(ZGivenCCW);
+        meanZGivenCW = mean(ZGivenCW);
+        varZGivenCCW = var(ZGivenCCW);
+        varZGivenCW = var(ZGivenCW);
+        
+        % Compute d-prime
+        dprime = (meanZGivenCCW-meanZGivenCW) / sqrt(0.5*(varZGivenCCW+varZGivenCW));
+        
+        
+        % Plot loglikelihood distributions of the two stimulus classes
+        figure(1); clf; set(gcf, 'Color', 'w', 'Position', [508, 820, 1052, 518]);
+        subplot(1,2,1); hold all;
+        [nA,xA] = hist(ZGivenCW,40);
+        bar(xA,nA, 'EdgeColor', 'k', 'FaceColor', 'w')
+        predA = length(ZGivenCW)*normpdf(xA,meanZGivenCW,sqrt(varZGivenCW))*(xA(2)-xA(1));
+        plot(xA,predA,'g','LineWidth',4);
+        set(gca, 'TickDir', 'out', 'FontSize', 14);
+        
+        [nB,xB] = hist(ZGivenCCW,40);
+        bar(xB,nB, 'EdgeColor', 'k', 'FaceColor', 'k');
+        predB = length(ZGivenCCW)*normpdf(xB,meanZGivenCCW,sqrt(varZGivenCCW))*(xB(2)-xB(1));
+        plot(xB,predB,'r','LineWidth',4);
+        title(sprintf('Probability of absorptions for Alpha vs Beta stim, contrast: %1.4f',c))
+        legend({'Alpha samples', 'Alpha fit', 'Beta samples', 'Beta fit'}, 'FontSize', 14, 'Location', 'best'); legend boxoff;
+        
+        % Get ROC curve from Hit and False Alarm rates and sample along many criteria
+        criteria = linspace(min([ZGivenCW,ZGivenCCW]),max([ZGivenCW,ZGivenCCW]),1000);
+        for cc = 1:length(criteria)
+            HitRate(cc) = length(find(ZGivenCCW > criteria(cc))) / length(ZGivenCCW);
+            FARate(cc) = length(find(ZGivenCW > criteria(cc))) / length(ZGivenCCW);
+        end
+        
+        % Get the percent correct
+        percentCorrect(c==expParams.contrastLevels) = -trapz([1 FARate 0],[1 HitRate 0]);
+        subplot(1,2,2);
+        plot([1 FARate 0],[1 HitRate 0],'r','LineWidth',4);
+        xlabel('False Alarm Rate');
+        ylabel('Hit Rate');
+        title(sprintf('ROC Curve, d-prime: %2.2f, percent corrent: %2.2f',dprime,percentCorrect(c==expParams.contrastLevels)*100));
+        axis('square'); axis([0 1 0 1]); set(gca, 'TickDir', 'out', 'FontSize', 14);
+        drawnow;
+        
+        
+        
+        % Report percent correct
+        fprintf('For contrast %1.4f: d-prime is %2.2f  percent correct %2.2f%%\n',c, dprime,percentCorrect(c==expParams.contrastLevels)*100);
+        
+        if saveFigs
+            savefig(fullfile(figurePth,sprintf('Geisler_Ideal_dprime_%s_c%1.4f.fig',expName,c)))
+            hgexport(gcf,fullfile(figurePth,sprintf('Geisler_Ideal_dprime_%s_c%1.4f.eps',expName,c)))
+        end
+    end
+    
+    saveFolderClassification = fullfile(ogRootPath, 'data', 'classification', expNameData, subFolderName);
+    fnameClassify = sprintf('ideal_Classify_coneOutputs_contrast%1.3f_pa0_eye00_eccen4.50_defocus0.00_noise-random_sf4.00_lms-0.60.30.1', max(expParams.contrastLevels));
+    accuracy = percentCorrect.*100;
+    parsave(fullfile(saveFolderClassification, sprintf('%s.mat', fnameClassify)),'accuracy',accuracy);
+    
+end
