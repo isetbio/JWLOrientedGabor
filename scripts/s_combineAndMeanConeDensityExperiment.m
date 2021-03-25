08b0001a008aa5c702658f5933868dea0e48ad07
elinekupers <elinekupers@gmail.com>
2021-03-25 16:00:11 -0400

    allow for stimtemplate data

--- scripts/s_combineAndMeanConeDensityExperiment.m
+++ scripts/s_combineAndMeanConeDensityExperiment.m
@@ -1,17 +1,29 @@
 %% s_combineAndMeanConeDensityExperiment
 
 %% 0. Set general experiment parameters
-expName                  = 'conedensity';
-expParams                = loadExpParams(expName, false);
+expName           = 'conedensity';
+stimTemplateFlag  = true;
+expParams         = loadExpParams(expName, false);
 [xUnits, colors, labels, M] = loadWeibullPlottingParams(expName);
 
-% Use cone current (flag = true) or cone absorptions (flag = false)
 polarAngles = expParams.polarAngle;
-% FFTflag     = true;
 
-% Where to find data and save figures
-dataPth     = fullfile(ogRootPath,'data','PF_data_alias','classification',expName);
-figurePth   = fullfile(ogRootPath,'figs', expName, 'average');
+% Where to find data
+if stimTemplateFlag 
+    subFolder = 'stimTemplate'; 
+    templateName = '_svmEnergy'; % choose from '_svmEnergy' or '_svmLinear'
+    baseFolder = '/Volumes/server/Projects/PerformanceFields_RetinaV1Model/';
+    dataPth     = fullfile(baseFolder,'data',expName,'classification','absorptions', subFolder);
+else 
+    templateName = [];
+    subFolder = '';
+    dataPth = fullfile(ogRootPath,'data','classification',expName);
+end
+
+% Where to save data and figures
+figurePth   = fullfile(ogRootPath,'figs', expName, ['average' templateName]);
+dataSavePth = fullfile(dataPth, ['average' templateName]);
+
 
 % Number of total trials in computational observer model (50 clockwise, 50 counterclockwise)
 nTotal      = 100;
@@ -26,7 +38,7 @@ lmsRatio    = expParams.cparams.spatialDensity;
 % First combine fovea
 for ec = 1:nrEccen
     
-    fName   = sprintf('Classify_coneOutputs_contrast%1.3f_pa%d_eye%s_eccen%1.2f_defocus%1.2f_noise-random_sf%1.2f_lms-%1.1f%1.1f%1.1f.mat', ...
+    fName   = sprintf('Classify_coneOutputs_contrast%1.4f_pa%d_eye%s_eccen%1.2f_defocus%1.2f_noise-random_sf%1.2f_lms-%1.1f%1.1f%1.1f.mat', ...
         max(expParams.contrastLevels),polarAngles,sprintf('%i',expParams.eyemovement'),expParams.eccentricities(ec),expParams.defocusLevels,expParams.spatFreq, lmsRatio(2),lmsRatio(3),lmsRatio(4));
     
     d = dir(fullfile(dataPth, 'run*'));
@@ -34,13 +46,20 @@ for ec = 1:nrEccen
     P =[];
     for ii = 1:size(d,1)
         
-        accuracy = load(fullfile(d(ii).folder, d(ii).name, fName));
-        accuracy.P = squeeze(accuracy.accuracy);
-        if size(accuracy.P,1)<size(accuracy.P,2)
-            accuracy.P = accuracy.P';
+        tmp = load(fullfile(d(ii).folder, d(ii).name, fName));
+        
+        if stimTemplateFlag
+            fn = fieldnames(tmp);
+            P_svm = tmp.(fn{strcmpi(fn,['P' templateName])});
+        else
+            P_svm = squeeze(tmp.accuracy);
         end
         
-        P = [P accuracy.P];
+        if size(P_svm,1)<size(P_svm,2)
+            P_svm = P_svm';
+        end
+        
+        P = [P P_svm];
         
     end
     
@@ -55,9 +74,9 @@ for ec = 1:nrEccen
         max(expParams.contrastLevels),polarAngles,sprintf('%i',expParams.eyemovement'),expParams.eccentricities(ec),expParams.defocusLevels,expParams.spatFreq, lmsRatio(2),lmsRatio(3),lmsRatio(4));
     
     
-    %         if ~exist(fullfile(dataPth, 'average'),'dir'), mkdir(fullfile(dataPth, 'average')); end;
-    %         save(fullfile(dataPth, 'average',fName),'P_AVG');
-    %         save(fullfile(dataPth, 'average',fNameSE),'P_SE');
+    if ~exist(dataSavePth,'dir'), mkdir(dataSavePth); end;
+    save(fullfile(dataSavePth, fName),'P_AVG');
+    save(fullfile(dataSavePth, fNameSE),'P_SE');
     
     %% Bootstrap runs with replacement,
     nboot = 1000;
@@ -92,8 +111,9 @@ end
 varThresh = std(ctrthresh,[],2);
 fNameSEThresh = sprintf('varThresh_coneResponse_absorptionrate_%d_conedensity.mat', ec);
 baseFolder = '/Volumes/server/Projects/PerformanceFields_RetinaV1Model';
-if ~exist(fullfile(baseFolder,'data',expName,'thresholds'), 'dir'); mkdir(fullfile(baseFolder,'data',expName,'thresholds')); end
-save(fullfile(baseFolder,'data',expName,'thresholds',fNameSEThresh),'varThresh', 'ctrthresh');
+if ~exist(fullfile(baseFolder,'data',expName,'thresholds',subFolder, ['stimTemplate' templateName]), 'dir'); 
+    mkdir(fullfile(baseFolder,'data',expName,'thresholds',subFolder, ['stimTemplate' templateName])); end
+save(fullfile(baseFolder,'data',expName,'thresholds',subFolder, ['stimTemplate' templateName], fNameSEThresh),'varThresh', 'ctrthresh');
 
 
 
