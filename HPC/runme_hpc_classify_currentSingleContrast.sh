9761e4b09c4d51b246640848b0afdc0638a821b1
elinekupers <elinekupers@gmail.com>
2021-04-13 14:02:50 -0400

    function to run foveal single contrast current classification on hpc

---
+++ HPC/runme_hpc_classify_currentSingleContrast.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time 10:00:00 # time (D-HH:MM)
+#SBATCH --mem=350GB # memory pool for all cores
+#SBATCH --job-name=Classify_current
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ek99@nyu.edu
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+
+module load matlab/2016b
+
+cd /scratch/ek99/JWLOrientedGabor/
+
+# If the files you are running are not in the same folder as this script,
+# you can insert "addpath(genpath('/PATH/TO/FILES/'));" before the command
+# you want to run.
+matlab -nodisplay -r "addpath(genpath('/scratch/ek99/JWLOrientedGabor')); addpath(genpath('/scratch/ek99/isetbio')); s_ogConeAbsorptions_ClassifyCurrent($SLURM_ARRAY_TASK_ID); exit()"
+
+exit
+
---
+++ scripts/s_ogConeAbsorptions_ClassifyCurrent.m
@@ -0,0 +1,126 @@
+function s_ogConeAbsorptions_Classify(c)
+%
+% Script to classify cone absorption rates for clockwise and counter-
+% clockwise oriented gabors simulated at different contrast levels.
+%
+% Absorption data are simulated with script: s_ogConeAbsorptions.m
+% using the ISETBIO toolbox.
+%
+% By Eline Kupers & Jonathan Winawer, NYU (2018)
+%
+%% Set params
+
+% Reset random number generator seed
+rng;
+
+% selectTimePoints = 1:109; % for absorptions use 1:28, for cone current use all;
+
+% Load experiment parameters
+expName = 'conedensity';
+for runNr = 1
+    subFolderName_toLoad = sprintf('run%d', runNr);
+    subFolderName_toSave = sprintf('run%d', runNr);
+    expParams = loadExpParams(expName);
+    noiseFlag = 'random'; % photon noise properties, saved in file name, could also be 'none' or 'random'
+    
+    % Compute accuracy for cone current as well
+    currentFlag    = true;
+    
+    % Compute accuracy on fft component of cone absorptions
+    fftFlag        = true;
+    
+    % Predefine matrix for predictions
+    if currentFlag
+        contrasts    = expParams.contrastLevelsPC;
+    else
+        contrasts    = expParams.contrastLevels;
+    end
+    nrContrasts      = length(contrasts);
+    nrEyemovTypes    = size(expParams.eyemovement,2);
+    nrEccen          = length(expParams.eccentricities);
+    nrSpatFreq       = length(expParams.spatFreq);
+    nrDefocusLevels  = length(expParams.defocusLevels);
+    
+    % Folder to save data
+    %     serverPth = '/Volumes/server-1/Projects/PerformanceFieldsIsetBio';
+    %      serverPth2 = '/Volumes/server-1/Projects/PerformanceFields_RetinaV1Model';
+    serverPth = '/scratch/ek99/JWLOrientedGabor/';
+    savePth = fullfile(serverPth2, 'data', expName, 'classification', 'current', subFolderName_toSave);
+    if ~exist('savePth', 'dir'); mkdir(savePth); end
+    
+    em = 1;
+    df = 1;
+    sf = expParams.spatFreq;
+    for eccen = 1:nrEccen
+        
+        % Load dataset
+        fname = sprintf(...
+            'OGconeOutputs_contrast%1.4f_pa%d_eye%s_eccen%1.2f_defocus%1.2f_noise-%s_sf%1.2f_lms-0.60.30.1.mat',...
+            contrasts(c),expParams.polarAngle,sprintf('%i',expParams.eyemovement(:,em)), expParams.eccentricities(eccen), expParams.defocusLevels(df), noiseFlag, sf);
+        
+        if currentFlag
+            fname = ['current_' fname];
+        end
+        pth = fullfile(serverPth, 'data', expName, subFolderName_toLoad, fname);
+        if ~exist(pth, 'file'), error('The file %s is not found', fname); end
+        tmp = load(pth);
+        
+        data = getfield(tmp,'current'); clear tmp;
+        
+        fprintf('Loading and classifying %s\n', fname);
+        % Get the trials and samples (should be the data for all data sets though
+        nStimuli = size(data,5);
+        nTrials  = size(data,1) * nStimuli/2;
+        tSamples = size(data,4);
+        nrows    = size(data,2);
+        ncols    = size(data,3);
+        % absorptions is trials x rows x cols x time points x stimuli
+        
+        %   permute to trials x stimuli x rows x cols x time points
+        data = permute(data, [1 5 2:4]);
+        
+        %   reshape to (trials x stimuli) x rows x cols x time points
+        data = reshape(data, [], nrows, ncols, tSamples);
+        
+        % permute to rows x cols x (trials x stimuli) x time points
+        data  = permute(data, [2 3 1 4]);
+        
+        % Compute fourier transform the cone array outputs
+        if fftFlag; data  = abs(fft2(data)); end
+        
+        % reshape to all trials x [rows x colums x time] for classification
+        data = permute(data, [3 1 2 4]);
+        data = reshape(data, nTrials*2, []);
+        
+        % permute the trial order within each of the two classes
+        idx = [randperm(nTrials) randperm(nTrials)+nTrials];
+        
+        data = data(idx, :);
+        
+        label = [ones(nTrials, 1); -ones(nTrials, 1)];
+        
+        % Fit the SVM model.
+        cvmdl = fitcsvm(data, label, 'Standardize', true, 'KernelFunction', 'linear', 'kFold', 10);
+        
+        % predict the data not in the training set.
+        classLoss = kfoldLoss(cvmdl);
+        
+        P = (1-classLoss) * 100;
+        disp(P);
+        
+        % Save classifier accuracy
+        fname = sprintf(...
+            'Classify_coneOutputs_contrast%1.4f_pa%d_eye%s_eccen%1.2f_defocus%1.2f_noise-random_sf%1.2f',...
+            contrasts(c), expParams.polarAngle,sprintf('%i',expParams.eyemovement(:,em)), expParams.eccentricities(eccen), expParams.defocusLevels(df), sf);
+        if currentFlag; fname = ['current_' fname]; end
+        parsave(fullfile(savePth, sprintf('%s.mat', fname)),'P',P)
+        
+    end
+end
+
+
+return
+
+
+
+
